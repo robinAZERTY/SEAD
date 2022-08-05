4f803086239b5539994d62a75aa5ee5d4bcc04f3
robin AZERTY <robinlecouffe@gmail.com>
2022-08-05 22:54:40 +0200

    hi

---
+++ sources/SEAD.cpp
@@ -0,0 +1,13 @@
+// SEAD.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
+
+
+#include "includes.h"
+
+
+int main()
+{
+    //print hello world
+    printf("Hello World!\n");
+    return 0;
+}
---
+++ sources/includes.h
@@ -0,0 +1,10 @@
+#ifndef INCLUDES_H
+#define INCLUDES_H
+
+//include all libraries you need here
+#include <stdio.h>
+#include <iostream>
+
+
+
+#endif // !INCLUDES_H
