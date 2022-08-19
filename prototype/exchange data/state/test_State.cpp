dbce97181d6b4aca439ebb76d0ae238f966b95aa
robin lecouffe <102579595+robinAZERTY@users.noreply.github.com>
2022-08-19 12:00:01 +0200

    update test_state

--- prototype/exchange data/state/test_State.cpp
+++ prototype/exchange data/state/test_State.cpp
@@ -6,7 +6,7 @@ void visualisation_CTOR_DTOR()
 {
     cout << "visualisation_CTOR_DTOR" << endl;
     State state;
-    cout << state.to_str() << endl;
+    cout << state.to_str() << endl;    
 }
 
 int main()
