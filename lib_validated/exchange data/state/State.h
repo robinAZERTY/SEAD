6359e357f4708775ebd662ddc5160f8e56dba928
robin lecouffe <102579595+robinAZERTY@users.noreply.github.com>
2022-08-19 12:01:23 +0200

    validate State

---
+++ lib_validated/exchange data/state/State.h
@@ -0,0 +1,60 @@
+/*
+créé par : robinAZERTY
+version du : 19/08/2022
+*/
+
+#pragma once
+
+#include <string>
+#include "Vector.cpp" // when this header is valid
+//#include "..\..\..\lib_validated\math\vector\Vector.cpp" // when this header is a prototype
+
+class PositionState
+{
+private:
+    string description = "default PositionState";
+
+public:
+    PositionState(){};
+    PositionState(PositionState &another);
+
+    void set_description(const string &description);
+    string to_str() const;
+
+    Vector position = Vector(3);
+    Vector velocity = Vector(3);
+    Vector acceleration = Vector(3);
+};
+
+class OrientationState
+{
+private:
+    string description = "default OrientationState";
+
+public:
+    OrientationState(){};
+    OrientationState(OrientationState &another);
+
+    void set_description(const string &description);
+    string to_str() const;
+
+    Vector orientation = Vector(4);
+    Vector velocity = Vector(4);
+    Vector acceleration = Vector(4);
+};
+
+class State
+{
+private:
+    string description = "default State";
+
+public:
+    State(){};
+    State(State &another);
+
+    PositionState positionState;
+    OrientationState orientationState;
+
+    void set_description(const string &description);
+    string to_str() const;
+};
