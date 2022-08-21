d0de19d01d68a9b6d5d3af0c7a97fbefda2e3c31
robin lecouffe <102579595+robinAZERTY@users.noreply.github.com>
2022-08-21 15:51:28 +0200

    OrientationMotionBase proto and test

---
+++ prototype/scenario construction/OrientationMotionBase/OrientationMotionBase.h
@@ -0,0 +1,35 @@
+/*
+créé par : RobinAZERTY
+version du : 20/08/2022 -1
+*/
+
+#pragma once
+
+//#include "State.h"//when this header is valid
+#include "..\..\../lib_validated/exchange data/state/State.cpp" //when this header is a prototype
+#include <string>
+
+class OrientationMotionBase
+{
+public:
+    OrientationMotionBase(){};
+    ~OrientationMotionBase(){};
+
+    virtual void set_parameters();
+    virtual string to_str();
+
+    void set_duration(const double &duration);
+
+    OrientationState get_state(const double &t);
+    OrientationState get_initial_state();
+    OrientationState get_final_state();
+
+protected:
+
+    virtual void update_state(const double &t);
+    virtual void init();
+    double duration = 0;
+    OrientationState initialState, state, finalState; // output
+    bool inited = false;
+    string description = "default OrientationMotionBase";
+};
