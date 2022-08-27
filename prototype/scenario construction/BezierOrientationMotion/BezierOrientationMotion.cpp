62f09baa6610754212ee5901cc15f2492ad32684
robin AZERTY <robinlecouffe@gmail.com>
2022-08-27 23:57:10 +0200

    #

--- prototype/scenario construction/BezierOrientationMotion/BezierOrientationMotion.cpp
+++ prototype/scenario construction/BezierOrientationMotion/BezierOrientationMotion.cpp
@@ -2,3 +2,36 @@
 créé par : robinAZERTY
 version du : 27/08/2022 - 0
 */
+
+#include "BezierOrientationMotion.h"
+
+BezierOrientationMotion::BezierOrientationMotion(const Quaternion q[4], const double &duration)
+{
+    qA = q[0];
+    qB = q[1];
+    qC = q[2];
+    qD = q[3];
+    this->duration = duration;
+}
+
+Slerp::Slerp(const Quaternion &q1, const Quaternion &q2)
+{
+    this->theta = acos(q1.b * q2.b + q1.c * q2.c + q1.d * q2.d + q1.a * q2.a);
+    this->sn = sin(theta);
+    this->q1 = q1;
+    this->q2 = q2;
+}
+const Quaternion Slerp::SLERP(const double &t) const
+{
+    Quaternion r;
+    double t_ = 1 - t;
+    double Wa, Wb;
+    Wa = sin(t_ * theta) / sn;
+    Wb = sin(t * theta) / sn;
+    r.b = Wa * q1.b + Wb * q2.b;
+    r.c = Wa * q1.c + Wb * q2.c;
+    r.d = Wa * q1.d + Wb * q2.d;
+    r.a = Wa * q1.a + Wb * q2.a;
+    r = r.normalize();
+    return r;
+}
--- prototype/scenario construction/BezierOrientationMotion/BezierOrientationMotion.h
+++ prototype/scenario construction/BezierOrientationMotion/BezierOrientationMotion.h
@@ -1,21 +1,35 @@
 /*
 créé par : robinAZERTY
 version du : 27/08/2022 - 0 obsolete par rapport à documents\Motions.pdf
+source : http://roboop.sourceforge.net/htmldoc/robotse9.html
 */
 
 #include "..\..\../lib_validated/scenario construction/OrientationMotionBase/OrientationMotionBase.cpp" //when this header is a prototype
 //#include "../OrientationMotionBase/OrientationMotionBase.cpp" //when this header is a valid
 
-#include "..\..\../lib_validated/math/quaternion/quaternion.cpp"//when this header is a prototype
+#include "..\..\../lib_validated/math/quaternion/quaternion.cpp" //when this header is a prototype
 //#include "..\../math/quaternion/quaternion.cpp" //when this header is a valid
-
 
 class BezierOrientationMotion : public OrientationMotionBase
 {
-    public :
-        BezierOrientationMotion();
-        BezierOrientationMotion(const Quaternion q[4], const double &duration);
+public:
+    BezierOrientationMotion();
+    BezierOrientationMotion(const Quaternion q[4], const double &duration);
 
-    private :
-       const Quaternion Slerp(const Quaternion &q1, const Quaternion &q2, const double &t) const;
+private:
+    Quaternion qA, qB, qC, qD;
+    Slerp Slerp_1, Slerp_2, Slerp_3;
+    Slerp Slerp_4, Slerp_5;
+    Slerp Slerp_6;
+};
+
+class Slerp
+{
+public:
+    Slerp(){};
+    Slerp(const Quaternion &q1, const Quaternion &q2);
+    Quaternion q1, q2;
+    double theta, sn;
+    const Quaternion SLERP(const double &t) const;
+    const Quaternion PRIME(const double &t) const;
 };
