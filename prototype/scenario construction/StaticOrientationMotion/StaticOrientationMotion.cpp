b18ea61ca36ebf0ececdfaabe36d12ff8207969f
robin AZERTY <robinlecouffe@gmail.com>
2022-08-27 22:48:10 +0200

    Static OrientationMotion update

--- prototype/scenario construction/StaticOrientationMotion/StaticOrientationMotion.cpp
+++ prototype/scenario construction/StaticOrientationMotion/StaticOrientationMotion.cpp
@@ -22,3 +22,35 @@ StaticOrientationMotion::StaticOrientationMotion(const OrientationState &Orienta
     this->finalState = state;
     this->description = "StaticOrientationMotion with Orientation input";
 }
+
+StaticOrientationMotion::StaticOrientationMotion(const Quaternion &q) : OrientationMotionBase()
+{
+    Quaternion qx, qy, qz;
+    qx.a = 0;
+    qy.a = 0;
+    qz.a = 0;
+
+    qx.b = 1;
+    qy.c = 1;
+    qz.d = 1;
+
+    Quaternion qE_x = q.rotate(qx);
+    Quaternion qE_y = q.rotate(qy);
+    Quaternion qE_z = q.rotate(qz);
+
+    state.E_x.set(0, qE_x.b);
+    state.E_x.set(1, qE_x.c);
+    state.E_x.set(2, qE_x.d);
+
+    state.E_y.set(0, qE_y.b);
+    state.E_y.set(1, qE_y.c);
+    state.E_y.set(2, qE_y.d);
+
+    state.E_z.set(0, qE_z.b);
+    state.E_z.set(1, qE_z.c);
+    state.E_z.set(2, qE_z.d);
+
+    initialState = state;
+    finalState = state;
+    this->description = "StaticOrientationMotion with Quaternion input";
+}
--- prototype/scenario construction/StaticOrientationMotion/StaticOrientationMotion.h
+++ prototype/scenario construction/StaticOrientationMotion/StaticOrientationMotion.h
@@ -8,9 +8,13 @@ version du : 20/08/2022
 #include "..\..\../lib_validated/scenario construction/OrientationMotionBase/OrientationMotionBase.cpp" //when this header is a prototype
 //#include "../OrientationMotionBase/OrientationMotionBase.cpp" //when this header is a valid
 
+#include "..\..\../lib_validated/math/quaternion/quaternion.cpp"//when this header is a prototype
+//#include "..\../math/quaternion/quaternion.cpp" //when this header is a valid
+
 class StaticOrientationMotion : public OrientationMotionBase
 {
 public:
     StaticOrientationMotion();
     StaticOrientationMotion(const OrientationState &OrientationState);
+    StaticOrientationMotion(const Quaternion &q);
 };
