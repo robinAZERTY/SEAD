5d304605e4a3ec34fbee5b20dc43ab1e41a17cc9
robin AZERTY <robinlecouffe@gmail.com>
2022-09-04 13:35:07 +0200

    j

--- prototype/sensors/sensor/sensor.h
+++ prototype/sensors/sensor/sensor.h
@@ -24,11 +24,12 @@ private:
     
     //features of the sensor
     string physical_quantities_observed;//acceleration,magnetisme,angular_velocity... in 1 direction !
-    double linearity(const double physical_quantities_value);//depend also of temperature range
+    double linearity(const double physical_quantities_value);//depend also of temperature range and operating_range
     double std_noise;
     double resolution;
     double lag_delay;
     double update_freq;
     double passband_freq[2]={0,9.9e100};
+    double operating_range[2]={-9.9e-100,9.9e100};
     double temperature_operating_range[2]={-9.9e-100,9.9e100};
 };
