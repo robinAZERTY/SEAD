b573b61e2670dcf479864f01584a3726d3372d6d
robin lecouffe <102579595+robinAZERTY@users.noreply.github.com>
2022-09-20 22:36:24 +0200

    Valid FIFO

--- prototype/tools/FIFO/FIFO_test.cpp
+++ prototype/tools/FIFO/FIFO_test.cpp
@@ -12,25 +12,32 @@ void visu()
 {
 
     FIFO_double fifo(10);
-    //remplir la fifo
+    //afficher la fifo
+    cout<<"initialisation de la fifo"<<endl;
     for (int i = 0; i < 10; i++)
+    {
+        cout<<"fifo["<<i<<"] = "<<fifo[i]<<endl;
+    }
+    cout<<endl;
+    //remplir la fifo
+    for (int i = 1; i <= 10; i++)
     {
         fifo.push(i);
     }
 
     //afficher la fifo
-    for (int i = 0; i > -10; i--)
+    for (int i = 0; i < 10; i++)
     {
         cout<<"fifo["<<i<<"] = "<<fifo[i]<<endl;
     }
 
     //add a new element
-    cout<<"add a new element"<<endl;
-    fifo.push(10);
+    cout<<"push a new element"<<endl;
+    fifo.push(11);
 
-    for(int i = 0; i > -10; i--)
+    for(unsigned short i = 0; i < 10; i++)
     {
-        cout<<"fifo["<<i<<"]  ="<<fifo[i]<<endl;
+        cout<<"fifo["<<i<<"]  ="<<fifo[i]<<"\t"<<fifo[-i]<<endl;
     }
     
 
