654c4398b74189a9d3aea3dbf14f3cdd4f7fc596
robin AZERTY <robinlecouffe@gmail.com>
2022-10-23 00:37:56 +0200

    a

--- mySIFT/imgFT.py
+++ mySIFT/imgFT.py
@@ -1,39 +1,77 @@
 """Fourier transform of an image.
 """
+from posixpath import pardir
 import numpy as np
 
-def FFT(signal,padding="zero"):
-    """
-    zero padding to the next power of 2
-    """
+def get_polynomial_padding(signal,n):
+    #useful to make periodic a signal
     
-    #if the signal is already a power of 2
+    x0=0
+    x1=n
+
+    y1=signal[0]
+    y0=signal[-1]
+    dy1=signal[1]-y1
+    dy0=y0-signal[-2] 
+    
+    Y=np.array([y0,y1,dy0,dy1])
+    A=np.array([[x0**3,x0**2,x0,1],[x1**3,x1**2,x1,1],[3*x0**2,2*x0,1,0],[3*x1**2,2*x1,1,0]])
+    X=np.linalg.solve(A,Y)
+    a,b,c,d=X
+    
+    ret=[a*(i+1)**3+b*(i+1)**2+c*(i+1)+d for i in range(n)]
+    
+    return ret
+
+def pad(signal,padding="zero"):
     N=len(signal)
     log2_N= np.log2(N)
-        
+    
     if log2_N%1==0:
-        ret=np.array(FFT_pow_of_2(signal))/N
-        return ret
+        return signal,N
     
     #find the next power of 2
     N2=2**int(np.ceil(log2_N))
-    #number of zero to add
-    n=N2-N
-    #zero padding
-    if padding=="zero":
-        pad=[0]*n
-    elif padding=="mirror":
-        pad=signal[::-1][0:n]
-    elif padding=="linear":
-        pad=[signal[-1]]*n
-    elif padding=="periodic":
-        pad=signal[:n]
-
-    else:
-        raise ValueError("padding not recognized")
     
-    signal=signal+pad
-    ret=np.array(FFT_pow_of_2(signal))/N2
+    #number of element to add
+    n=N2-N
+    
+    padding_switcher={
+            "zero":[0]*n,
+            "linear":[signal[-1]]*n,
+            "mirror":signal[-n:][::-1],
+            "periodic":signal[:n],
+            "polynomial":get_polynomial_padding(signal,n)
+            }
+    
+    pad=padding_switcher[padding]
+    new_signal=np.concatenate((signal,pad))
+    return new_signal,N2
+
+
+
+
+    
+    
+    
+
+def depad(signal,N):
+    n=len(signal)
+    if n==N:
+        return signal
+    
+    nl=(n-N)//2
+    if N%2==0:
+        nr=nl
+    else:
+        nr=nl-1
+        
+    return signal[nl:n-nr]
+    
+
+def FFT(signal,padding="zero"):
+    signal,N=pad(signal,padding)
+    ret=np.array(FFT_pow_of_2(signal))/N
     return ret
 
 def FT_get_half(coeff):
@@ -54,6 +92,8 @@ def FT_get_entier(coeff):
     #pop the last element
     ret=ret[:-1]
     return ret
+
+    
 
 def FFT_pow_of_2(signal):
     """_summary_
@@ -117,3 +157,47 @@ def IFFT_pow_of_2(coeff):
         odd=IFFT(coeff[1::2])
         T=[np.exp(2j*np.pi*k/N)*odd[k] for k in range(N//2)]
         return [(even[k]+T[k]) for k in range(N//2)]+[(even[k]-T[k]) for k in range(N//2)]
+    
+    
+    
+"""
+Only with half of the coefficient
+"""
+
+
+    
+def FT_list(ft):
+    ret=[np.abs(ft),ft,[i for i in range(len(ft))]]
+    #transpose the matrix
+    ret=np.transpose(ret)
+    return ret
+
+    
+def FT_e_sum(fte1,fte2):
+    return [fte1[0]*fte2[0],fte1[1]+fte2[1]]
+
+# take second element for sort
+def takeFirst(elem):
+    return elem[0]
+
+
+
+def compress(ft,treshold_coeff):
+    hft=FT_get_half(ft)#ok
+    ft_l=FT_list(hft)
+    if treshold_coeff==0:
+        return ft_l
+    #compute the average
+    avg=np.average(ft_l[:,0])
+    #keep only the coefficient above the treshold
+    tres=avg*treshold_coeff
+    ft_c=ft_l[ft_l[:,0]>tres]
+    return ft_c
+
+def decompress(ft_c,N):
+    half=[0j+0]*(N//2)
+    for _,z,i in ft_c:
+        index=int(i.real)
+        half[index]=z
+    ret=FT_get_entier(half)
+    return ret
--- mySIFT/testZone.py
+++ mySIFT/testZone.py
@@ -1,82 +1,42 @@
 from imgFT import*
 import matplotlib.pyplot as plt
 
-def set_axes_equal(ax):
-    '''Make axes of 3D plot have equal scale so that spheres appear as spheres,
-    cubes as cubes, etc..  This is one possible solution to Matplotlib's
-    ax.set_aspect('equal') and ax.axis('equal') not working for 3D.
+N=2**12
+cut=N//10
+t=np.linspace(0,1,N)
+t=t[0:N-cut]
+N=len(t)
+w=2*np.pi*t
+signal = np.cos(w*1.8)+0.5*np.cos(5*w)-1
+#add noise
+signal+=np.random.normal(0,0.0,N)
+#signal=np.exp(-((t-0.5)/0.03)**2/0.1)
+ft=FFT(signal,padding="polynomial")
+#hft=FT_get_half(ft)
+#ft2=FT_get_entier(hft)
 
-    Input
-      ax: a matplotlib axis, e.g., as output from plt.gca().
-    '''
-
-    x_limits = ax.get_xlim3d()
-    y_limits = ax.get_ylim3d()
-    z_limits = ax.get_zlim3d()
-
-    x_range = abs(x_limits[1] - x_limits[0])
-    x_middle = np.mean(x_limits)
-    y_range = abs(y_limits[1] - y_limits[0])
-    y_middle = np.mean(y_limits)
-    z_range = abs(z_limits[1] - z_limits[0])
-    z_middle = np.mean(z_limits)
-
-    # The plot bounding box is a sphere in the sense of the infinity
-    # norm, hence I call half the max range the plot radius.
-    plot_radius = 0.5*max([x_range, y_range, z_range])
-
-    ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
-    ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
-    ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
-    
-#buid a step function
-def step(x,x1):
-    if x < x1:
-        return 0
-    else:
-        return 1
-    
-
-#signal = [step(x,32) for x in range(64)]
-#signal = [np.sin(2+10*x/128*(2*np.pi)+1) for x in range(128)]
-signal = [np.exp(-((x-64)/2)**2/2) for x in range(128)]
-ft=FFT(signal,"mirror")
-
-
-
-half=FT_get_half(ft)
-#we can work with the half of the fourier transform
-n=25#number of coefficient to keep
-initailLength=len(half)
-half=half[0:n-1]
-half=np.concatenate((half,[0]*(initailLength-n+1)))
-entier=FT_get_entier(half)
-
+#ift=depad(IFFT(ft2),N)
 ift=IFFT(ft)
-ift2=IFFT(entier)
+N2=len(ift)
+print(N,N2)
+fte=compress(ft,1)
+
+ftd=decompress(fte,N2)
+ift2=IFFT(ftd)
+
+
+
 
 #plot the signal
+plt.figure()
 plt.subplot(2,1,1)
 plt.plot(signal)
 plt.plot(ift)
 plt.plot(ift2)
-plt.legend(["signal","FFT","FFT with half"])
-plt.subplot(2,2,3)
-plt.plot(np.abs(ft))
-plt.plot(np.angle(ft))
-plt.legend(["abs","angle"])
-plt.subplot(2,2,4)
-plt.plot(np.abs(entier))
-plt.plot(np.angle(entier))
-plt.legend(["abs","angle"])
-plt.title("half reconstructed")
+plt.legend(["true","ifft k= "+str(len(ft)),"compressed k = "+str(len(fte))])
+plt.subplot(2,1,2)
+plt.plot(abs(ft))
+plt.plot(abs(ftd))
+plt.legend(["original","decompressed"])
 
-
-fig = plt.figure()
-ax = plt.axes(projection ='3d')
-
-
-ax.plot3D(np.real(ft),np.imag(ft),np.linspace(0, 1, len(ft)))
-set_axes_equal(ax)
 plt.show()
-
